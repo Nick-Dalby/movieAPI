0a70dd42dc24a4941d2bc07a4fad1ecafc3a8af8
Nick Dalby <nckdlby@gmail.com>
2022-04-13 13:58:00 +0200

    mongoose connect uri updated

--- index.js
+++ index.js
@@ -29,7 +29,12 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/moviesDB', {
+// mongoose.connect('mongodb://localhost:27017/moviesDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
